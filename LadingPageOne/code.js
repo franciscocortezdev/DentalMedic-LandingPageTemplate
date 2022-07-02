08a55850c0f3d2e90f47a4b29db80d95e1708820
Francisco Cortez <franciscocortezdev@gmail.com>
2022-07-02 10:44:06 -0600

    Question section done

--- LadingPageOne/code.js
+++ LadingPageOne/code.js
@@ -1,6 +1,8 @@
 const sliders = [...document.querySelectorAll('.testimonyBody')]
+const titleQuestions = [...document.querySelectorAll('.questionTitle')]
 const beforeSlide = document.getElementById('leftArrow')
 const nextSlide = document.getElementById('rightArrow')
+
 let value = 0
 
 nextSlide.addEventListener('click', () => {
@@ -16,18 +18,27 @@ const changePosition = (slide)=>{
   value = Number(currentSlide)
   value += Number(slide)
 
-
-
   value = value === 0 ? sliders.length : value > sliders.length ? 1 : value
   sliders[Number(currentSlide)-1].classList.remove('testimonyBodyShow')
   sliders[value-1].classList.add('testimonyBodyShow')
-
-  
-
 }
 
+titleQuestions.forEach(question => {
+  question.addEventListener('click', ()=>{
+    let height = 0;
+    let answer = question.nextElementSibling;
+    let addPadding = question.parentElement.parentElement
 
+    addPadding.classList.toggle('questionPaddingAdd')
+    question.children[0].classList.toggle('questionArrowRotate')
 
+    if(answer.clientHeight === 0){
+      height = answer.scrollHeight
+
+    }
+    answer.style.height = `${height}px`
+  })
+})
 
 
 
